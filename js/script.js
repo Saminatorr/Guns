2efe9533ff904a0f1460fc7d3d774b63e12261e6
Saminatorr <sam.007@hotmail.com>
2014-12-22 10:41:32 +0100

    Flow van de website werkt. Nu nog design, inhoud, ...

--- js/script.js
+++ js/script.js
@@ -1,4 +1,72 @@
-$(document).ready(function () {
-    console.log("hello");
-    $('#fullpage').fullpage();
-});
+document.addEventListener("DOMContentLoaded", init);
+
+function init() {
+    getViewportData();
+
+    var HomeButton = document.getElementById("HomeButton");
+    HomeButton.addEventListener("click", goToSection1);
+
+    var Button1300 = document.getElementById("Button1");
+    Button1300.addEventListener("click", goToSection2);
+
+    var Button1400 = document.getElementById("Button2");
+    Button1400.addEventListener("click", goToSection3);
+}
+
+function getViewportData() {
+    var heightScreen, widthScreen;
+
+    heightScreen = $(window).height();
+    widthScreen = $(window).width();
+
+    $("#Section1").css({ 'height': heightScreen });
+    $("#Section1").css({ 'width': widthScreen });
+
+    $("#Section2").css({ 'height': heightScreen });
+    $("#Section2").css({ 'width': widthScreen });
+
+    $("#Section3").css({ 'height': heightScreen });
+    $("#Section3").css({ 'width': widthScreen });
+}
+
+function goToSection1() {
+    $('html, body').animate({
+        scrollTop: $("#Section1").offset().top
+    }, 500);
+
+    var x = document.getElementsByClassName("button")
+    for (var i = 0; i < x.length; i++) {
+        $(x[i]).delay(200).queue(function (next) {
+            $(this).css({ 'background-color': '#2980b9' });
+            next();
+        });
+    }
+}
+
+function goToSection2() {
+    $('html, body').animate({
+        scrollTop: $("#Section2").offset().top
+    }, 500);
+
+    var x = document.getElementsByClassName("button")
+    for (var i = 0; i < x.length; i++) {
+        $(x[i]).delay(200).queue(function (next) {
+            $(this).css({ 'background-color': '#e74c3c' });
+            next();
+        });
+    }
+}
+
+function goToSection3() {
+    $('html, body').animate({
+        scrollTop: $("#Section3").offset().top
+    }, 500);
+
+    var x = document.getElementsByClassName("button")
+    for (var i = 0; i < x.length; i++) {
+        $(x[i]).delay(200).queue(function (next) {
+            $(this).css({ 'background-color': 'yellow' });
+            next();
+        });
+    }
+}
